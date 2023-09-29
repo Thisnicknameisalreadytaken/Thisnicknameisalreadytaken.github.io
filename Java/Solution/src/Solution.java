4fdec534f94290a5652fed49e876e6d703987d5a
gitsgq <97238307+Thisnicknameisalreadytaken@users.noreply.github.com>
2023-09-30 02:13:05 +0800

    renew

--- Java/Solution/src/Solution.java
+++ Java/Solution/src/Solution.java
@@ -1,27 +1,34 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Solution {
-    public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int res = 0;
-        if (n == 0)
-            return true;
-        int len = flowerbed.length;
-        if (len == 1)
-            return flowerbed[0] == 0 && n == 1;
-        else if (len == 2)
-            return flowerbed[0] + flowerbed[1] == 0 && n == 1;
-        if (flowerbed[0] + flowerbed[1] == 0) {
-            ++res;
-            flowerbed[0] = 1;
+    public int earliestFullBloom(int[] plantTime, int[] growTime) {
+        int len = plantTime.length;
+        int res[] = new int[len];
+        int time[][] = new int[len][2];
+        int ans = 0;
+        for (int i = 0; i < len; ++i) {
+            time[i][0] = plantTime[i] + growTime[i];
+            time[i][1] = growTime[i];
         }
-        if (flowerbed[len - 1] + flowerbed[len - 2] == 0) {
-            ++res;
-            flowerbed[len - 1] = 1;
-        }
-        for (int i = 1; i < len - 1; ++i) {
-            if (flowerbed[i - 1] + flowerbed[i] + flowerbed[i + 1] == 0) {
-                ++res;
-                flowerbed[i] = 1;
+        Arrays.sort(time, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] != o2[0] ? o2[0] - o1[0] : o2[1] - o1[1];
             }
+        });
+        int day = 0;
+        for (int i = 0; i < len; ++i) {
+            res[i] = Math.max(res[i], day + time[i][0]);
+            day += (time[i][0] - time[i][1]);
         }
-        return res >= n;
+        ans = res[len - 1];
+        for (int i = len - 1; i >= 0; --i) {
+            if (res[i] - time[i][0] > res[i - 1] + time[i][0] - time[i][1]) {
+                ans = res[i - 1] + time[i][0] - time[i][1];
+            } else
+                break;
+        }
+        return ans;
     }
 }

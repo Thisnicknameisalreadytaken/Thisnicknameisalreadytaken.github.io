641b10dea7eb240cbfd0240511327d4bc8d5e50e
gitsgq <97238307+Thisnicknameisalreadytaken@users.noreply.github.com>
2023-10-04 16:06:37 +0800

    renew

--- Java/Solution/src/Solution.java
+++ Java/Solution/src/Solution.java
@@ -1,34 +1,39 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 class Solution {
-    public int earliestFullBloom(int[] plantTime, int[] growTime) {
-        int len = plantTime.length;
-        int res[] = new int[len];
-        int time[][] = new int[len][2];
-        int ans = 0;
-        for (int i = 0; i < len; ++i) {
-            time[i][0] = plantTime[i] + growTime[i];
-            time[i][1] = growTime[i];
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        int len = prices.length;
+        int judge = 0;
+        for (int i = 0; i < len - 1; ++i) {
+            if (prices[i] > prices[i + 1])
+                judge = 1;
         }
-        Arrays.sort(time, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] != o2[0] ? o2[0] - o1[0] : o2[1] - o1[1];
+        if (judge == 0)
+            return prices[len - 1] - prices[0];
+        int min1 = 0x3f3f3f3f;
+        int min2 = 0;
+        int max1 = 0;
+        int max2 = 0;
+        int temp1 = 0, temp2 = 0;
+        for (int i = 0; i < len; ++i) {
+            min1 = Math.min(min1, prices[i]);
+            max1 = Math.max(max1, prices[i] - min1);
+            if (temp1 < i || temp2 < i) {
+                min2 = i;
+                max2 = 0;
+                temp1 = 0;
+                temp2 = 0;
+                for (int j = i; j < len; ++j) {
+                    if (prices[min2] > prices[j])
+                        min2 = j;
+                    else if (max2 < prices[j] - prices[min2]) {
+                        temp1 = min2;
+                        temp2 = j;
+                        max2 = prices[j] - prices[min2];
+                    }
+                }
             }
-        });
-        int day = 0;
-        for (int i = 0; i < len; ++i) {
-            res[i] = Math.max(res[i], day + time[i][0]);
-            day += (time[i][0] - time[i][1]);
+            res = Math.max(res, max1 + prices[temp2] - prices[temp1]);
         }
-        ans = res[len - 1];
-        for (int i = len - 1; i >= 0; --i) {
-            if (res[i] - time[i][0] > res[i - 1] + time[i][0] - time[i][1]) {
-                ans = res[i - 1] + time[i][0] - time[i][1];
-            } else
-                break;
-        }
-        return ans;
+        return res;
     }
 }
--- Java/Solution/src/SolutionTest.java
+++ Java/Solution/src/SolutionTest.java
@@ -3,5 +3,8 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class SolutionTest {
-
+    @Test
+    public void test() {
+        assertEquals(7, new Solution().maxProfit(new int[] { 3, 2, 6, 5, 0, 3 }));
+    }
 }

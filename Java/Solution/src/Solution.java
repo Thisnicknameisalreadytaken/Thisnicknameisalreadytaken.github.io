d0724f54f2d4a869c99f32a123c4aa2a3eb9b067
gitsgq <97238307+Thisnicknameisalreadytaken@users.noreply.github.com>
2023-10-07 11:29:51 +0800

    renew

--- Java/Solution/src/Solution.java
+++ Java/Solution/src/Solution.java
@@ -1,18 +1,25 @@
-class Solution {
-    public int maxProfit(int[] prices) {
-        int len = prices.length;
-        if (len <= 1)
-            return 0;
-        int buy[] = new int[len];
-        int sell[] = new int[len];
-        buy[0] = -prices[0];
-        sell[0] = 0;
-        buy[1] = Math.max(-prices[0], -prices[1]);
-        sell[1] = Math.max(0, buy[0] - prices[1]);
-        for (int i = 2; i < len; ++i) {
-            buy[i] = Math.max(buy[i - 1], sell[i - 2] - prices[i]);
-            sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+class StockSpanner {
+
+    public StockSpanner() {
+
+    }
+
+    int num[] = new int[10002];
+    int len = 0;
+
+    public int next(int price) {
+        num[len++] = price;
+        int res = 0;
+        int max = 0;
+        for (int i = len - 1; i >= 0; --i) {
+            if (price >= num[i]) {
+                ++max;
+            } else {
+                res = Math.max(res, max);
+                max = 0;
+            }
         }
-        return sell[len - 1];
+        res = Math.max(res, max);
+        return res;
     }
 }

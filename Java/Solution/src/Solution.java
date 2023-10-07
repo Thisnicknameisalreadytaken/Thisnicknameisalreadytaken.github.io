5f016f059ebbb4194ef7b206a5d2816940b0fbaa
gitsgq <97238307+Thisnicknameisalreadytaken@users.noreply.github.com>
2023-10-08 01:37:15 +0800

    renew

--- Java/Solution/src/Solution.java
+++ Java/Solution/src/Solution.java
@@ -1,25 +1,50 @@
-class StockSpanner {
+import java.util.HashMap;
 
-    public StockSpanner() {
+class StockPrice {
+
+    public StockPrice() {
 
     }
 
-    int num[] = new int[10002];
-    int len = 0;
+    int max = -0x3f3f3f3f;
+    int min = 0x3f3f3f3f;
+    int newtime = 0;
+    HashMap<Integer, Integer> hash = new HashMap<>();
 
-    public int next(int price) {
-        num[len++] = price;
-        int res = 0;
-        int max = 0;
-        for (int i = len - 1; i >= 0; --i) {
-            if (price >= num[i]) {
-                ++max;
+    public void update(int timestamp, int price) {
+        boolean judge = hash.containsKey(timestamp);
+        if (judge == false) {
+            hash.put(timestamp, price);
+            newtime = Math.max(timestamp, newtime);
+            max = Math.max(max, price);
+            min = Math.min(min, price);
+        } else {
+            if (hash.get(timestamp) == max || hash.get(timestamp) == min) {
+                max = -0x3f3f3f3f;
+                min = 0x3f3f3f3f;
+                hash.put(timestamp, price);
+                for (int i : hash.values()) {
+                    max = Math.max(max, i);
+                    min = Math.min(min, i);
+                }
             } else {
-                res = Math.max(res, max);
-                max = 0;
+                hash.put(timestamp, price);
+                max = Math.max(max, price);
+                min = Math.min(min, price);
             }
         }
-        res = Math.max(res, max);
-        return res;
+
+    }
+
+    public int current() {
+        return hash.get(newtime);
+    }
+
+    public int maximum() {
+        return max;
+    }
+
+    public int minimum() {
+        return min;
     }
 }
